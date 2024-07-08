59125c41455523834b6d1ef7968b2bfd62dd0937
Piper <heather.a.piper@gmail.com>
2024-07-08 19:12:37 -0400

    Update version bump script

--- version-bump.mjs
+++ version-bump.mjs
@@ -7,8 +7,3 @@ let manifest = JSON.parse(readFileSync("manifest.json", "utf8"));
 const { minAppVersion } = manifest;
 manifest.version = targetVersion;
 writeFileSync("manifest.json", JSON.stringify(manifest, null, "\t"));
-
-// update versions.json with target version and minAppVersion from manifest.json
-let versions = JSON.parse(readFileSync("versions.json", "utf8"));
-versions[targetVersion] = minAppVersion;
-writeFileSync("versions.json", JSON.stringify(versions, null, "\t"));
