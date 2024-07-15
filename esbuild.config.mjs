c323de2e8e14bb3239a6fd95fad537a8d1006c20
Piper <heather.a.piper@gmail.com>
2024-07-14 23:23:55 -0400

    Bump version number

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -37,5 +37,5 @@ esbuild.build({
 	logLevel: "info",
 	sourcemap: prod ? false : 'inline',
 	treeShaking: true,
-	outfile: 'dist/main.js',  // Changed this line
+	outfile: 'dist/main.js',
 }).catch(() => process.exit(1));
