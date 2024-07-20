45fb0222bb0bd04ac43efd1321a167b2380c27c4
Piper <heather.a.piper@gmail.com>
2024-07-20 01:21:54 -0400

    Contrast border setting will only appear if the highlight style is set to 'background color.'

--- src/settings.ts
+++ src/settings.ts
@@ -105,25 +105,27 @@ export class ColorHighlighterSettingTab extends PluginSettingTab {
                     this.display();
                 })
             );
-        
+
+        if (this.plugin.settings.highlightStyle === 'background') {
+            new Setting(containerEl)
+                .setName('Use contrasting border for low-contrast highlights')
+                .setDesc('Adds a faint border around highlights when there is not enough contrast with the background.')
+                .addToggle(toggle => toggle
+                    .setValue(this.plugin.settings.useContrastingBorder)
+                    .onChange(async (value) => {
+                        this.plugin.settings.useContrastingBorder = value;
+                        await this.plugin.saveSettings();
+                    })
+                );
+            }
+
         new Setting(containerEl)
             .setName('Enable color picker on hover')
-            .setDesc('Show color picker when hovering over highlighted color codes. If disabled, you may still show the color picker by using the command palette.')
+            .setDesc('Show color picker when hovering over highlighted color codes. If disabled, you may still show the color picker by selecting "Color Highlighter: Show color picker" in the command palette.')
             .addToggle(toggle => toggle
                 .setValue(this.plugin.settings.enableColorPicker)
                 .onChange(async (value) => {
                     this.plugin.settings.enableColorPicker = value;
-                    await this.plugin.saveSettings();
-                })
-            );
-
-        new Setting(containerEl)
-            .setName('Use contrasting border for low-contrast highlights')
-            .setDesc('Adds a faint border around highlights when there is not enough contrast with the background.')
-            .addToggle(toggle => toggle
-                .setValue(this.plugin.settings.useContrastingBorder)
-                .onChange(async (value) => {
-                    this.plugin.settings.useContrastingBorder = value;
                     await this.plugin.saveSettings();
                 })
             );
